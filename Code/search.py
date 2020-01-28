c3f72ce08eee153ab1a13b33e44c287216de48c8
HexSeal <maxfin13@gmail.com>
2020-01-27 20:38:56 -0800

    Attempted linear and binary search algorithms

--- Code/search.py
+++ Code/search.py
@@ -4,8 +4,8 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    # return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -17,11 +17,12 @@ def linear_search_iterative(array, item):
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
-
+    if index >= len(array):
+        return None
+    elif item != array[index]:
+        linear_search_recursive(array, item, index + 1)
+    else:
+        return index
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
@@ -32,8 +33,19 @@ def binary_search(array, item):
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
+    half_array = int(len(array)/2)
+    # Start at the beginning of the step
+    start = array[half_array]
+    
+    for _ in array:
+        # if it's smaller than the target, cut the array search in half choose the top one
+        if array.index(start) < array.index(item):
+            start = array[start:]
+        # if it's bigger go down
+        if array.index(start) > array.index(item):
+            start = array[:start]
+        else:
+            return item
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
